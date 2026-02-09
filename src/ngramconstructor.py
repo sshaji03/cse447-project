04e72efafcad747d8068c5a3f938e9ac118cdbc9
Shreya Shaji <sshaji@uw.edu>
2026-02-09 12:10:14 -0800

    handling for five gram

--- src/ngramconstructor.py
+++ src/ngramconstructor.py
@@ -1,13 +1,15 @@
 import pandas as pd
 import math
+import os
 
 N_GRAM_FOLDER = "work/hashtable_counts/"
-BIGRAM_DEV = N_GRAM_FOLDER + "bigrams_dev.csv"
-BIGRAM_TRAIN = N_GRAM_FOLDER + "bigrams_train.csv"
+FIVEGRAM_DEV = N_GRAM_FOLDER + "5grams_dev.csv"
+FIVEGRAM_TRAIN = N_GRAM_FOLDER + "/split_5grams_train/"
 TRIGRAM_DEV = N_GRAM_FOLDER + "trigrams_dev.csv"
 TRIGRAM_TRAIN = N_GRAM_FOLDER + "trigrams_train.csv"
 UNIGRAM_DEV = N_GRAM_FOLDER + "unigrams_dev.csv"
 UNIGRAM_TRAIN = N_GRAM_FOLDER + "unigrams_train.csv"
+UNK_CHAR = "\uE000"
 
 class NgramConstructor:
     """
@@ -18,8 +20,8 @@ class NgramConstructor:
         self.unigram_train = dict()
         self.unigram_dev = dict()
         self.unigram_total = 0
-        self.bigrams_train = dict()
-        self.bigrams_dev = dict()
+        self.fivegrams_train = dict()
+        self.fivegrams_dev = dict()
         self.trigrams_train = dict()
         self.trigrams_dev = dict()
 
@@ -29,23 +31,22 @@ class NgramConstructor:
         self.unigram_token_total_train = 0
         self.unigram_token_total_dev = 0
         # counts how many bigram tokens start with a specific character
-        self.bigrams_token_counts_train = dict()
-        self.bigrams_token_counts_dev = dict()
+        self.fivegrams_token_counts_train = dict()
+        self.fivegrams_token_counts_dev = dict()
         # counts how many trigram tokens start with a specific character
         self.trigram_token_counts_train = dict()
         self.trigram_token_counts_dev = dict()
 
         self.unigram_prob_train = dict()
         self.unigram_prob_dev = dict()
-        self.bigram_prob_train = dict()
-        self.bigram_prob_dev = dict()
+        self.fivegram_prob_train = dict()
+        self.fivegram_prob_dev = dict()
         self.trigram_prob_train = dict()
         self.trigram_prob_dev = dict()
 
         unigram_df_train = pd.read_csv(UNIGRAM_TRAIN, dtype={'char_ngram': str})
         unigram_df_dev = pd.read_csv(UNIGRAM_DEV, dtype={'char_ngram': str})
-        bigram_df_train = pd.read_csv(BIGRAM_TRAIN, dtype={'char_ngram': str})
-        bigram_df_dev = pd.read_csv(BIGRAM_DEV, dtype={'char_ngram': str})
+        fivegram_df_dev = pd.read_csv(FIVEGRAM_DEV, dtype={'char_ngram': str})
         trigram_df_train = pd.read_csv(TRIGRAM_TRAIN, dtype={'char_ngram': str})
         trigram_df_dev = pd.read_csv(TRIGRAM_DEV, dtype={'char_ngram': str})
 
@@ -68,21 +69,32 @@ class NgramConstructor:
             else:
                 self.unigram_dev[unigram] = count
 
-        for _, row in bigram_df_train.iterrows():
-            bigram = str(row['char_ngram'])
+        files = os.listdir(FIVEGRAM_TRAIN)
+        for file in files:
+            path = os.path.join(FIVEGRAM_TRAIN, file)
+            csv = pd.read_csv(path, dtype={'char_ngram': str})
+            for _, row in csv.iterrows():
+                fivegram = str(row['char_ngram'])
+                if len(fivegram) < 5:
+                    continue
+                count = row['count']
+                self.fivegrams_train[fivegram] = self.fivegrams_train.get(fivegram, 0) + count
+
+                c1, c2, c3, c4, c5 = fivegram
+                context = c1 + c2 + c3 + c4
+                self.fivegrams_token_counts_train[context] = self.fivegrams_token_counts_train.get(context, 0) + count
+
+        for _, row in fivegram_df_dev.iterrows():
+            fivegram = str(row['char_ngram'])
+            if len(fivegram) < 5:
+                continue
+
             count = row['count']
-            self.bigrams_train[bigram] = self.bigrams_train.get(bigram, 0) + count
+            self.fivegrams_dev[fivegram] = self.fivegrams_dev.get(fivegram, 0) + count
 
-            c1, c2 = bigram
-            self.bigrams_token_counts_train[c1] = self.bigrams_token_counts_train.get(c1, 0) + count
-
-        for _, row in bigram_df_dev.iterrows():
-            bigram = str(row['char_ngram'])
-            count = row['count']
-            self.bigrams_dev[bigram] = self.bigrams_dev.get(bigram, 0) + count
-
-            c1, c2 = bigram
-            self.bigrams_token_counts_dev[c1] = self.bigrams_token_counts_dev.get(c1, 0) + count
+            c1, c2, c3, c4, c5 = fivegram
+            context = c1 + c2 + c3 + c4
+            self.fivegrams_token_counts_dev[context] = self.fivegrams_token_counts_dev.get(context, 0) + count
 
         for _, row in trigram_df_train.iterrows():
             trigram = str(row['char_ngram'])
@@ -120,13 +132,15 @@ class NgramConstructor:
         for unigram in self.unigram_dev.keys():
             self.unigram_prob_dev[unigram] = self.unigram_dev[unigram] / self.unigram_token_total_dev
 
-        for bigram in self.bigrams_train.keys():
-            c1, _ = bigram
-            self.bigram_prob_train[bigram] = self.bigrams_train[bigram] / self.bigrams_token_counts_train[c1]
+        for fivegram in self.fivegrams_train.keys():
+            c1, c2, c3, c4, _ = fivegram
+            context = c1 + c2 + c3 + c4
+            self.fivegram_prob_train[fivegram] = self.fivegrams_train[fivegram] / self.fivegrams_token_counts_train[context]
 
-        for bigram in self.bigrams_dev.keys():
-            c1, _ = bigram
-            self.bigram_prob_dev[bigram] = self.bigrams_dev[bigram] / self.bigrams_token_counts_dev[c1]
+        for fivegram in self.fivegrams_dev.keys():
+            c1, c2, c3, c4, _ = fivegram
+            context = c1 + c2 + c3 + c4
+            self.fivegram_prob_dev[fivegram] = self.fivegrams_dev[fivegram] / self.fivegrams_token_counts_dev[context]
 
         for trigram in self.trigrams_train.keys():
             c1, c2, _ = trigram
@@ -144,11 +158,11 @@ class NgramConstructor:
     def get_unigram_dev(self):
         return self.unigram_dev
 
-    def get_bigram_train(self):
-        return self.bigrams_train
+    def get_fivegram_train(self):
+        return self.fivegrams_train
 
-    def get_bigram_dev(self):
-        return self.bigrams_dev
+    def get_fivegram_dev(self):
+        return self.fivegrams_dev
 
     def get_trigram_train(self):
         return self.trigrams_train
@@ -162,40 +176,40 @@ class NgramConstructor:
     def calculate_train_perplexity(self, lambda1, lambda2, lambda3):
         log_prob_sum = 0.0
 
-        for trigram, count in self.trigrams_train.items():
-            unigram, c2, c3 = trigram
-            bigram = unigram+c2
+        for fivegram, count in self.fivegrams_train.items():
+            c1, c2, c3, c4, unigram = fivegram
+            trigram = c3 + c4 + unigram
+            unigram = unigram if unigram in self.vocab else UNK_CHAR
 
             uni = lambda1 * self.unigram_prob_train[unigram]
-            bi = lambda2 * self.bigram_prob_train[bigram]
-            tri = lambda3 * self.trigram_prob_train[trigram]
+            tri = lambda2 * self.trigram_prob_train[trigram]
+            five = lambda3 * self.fivegram_prob_train[fivegram]
 
-            log_prob_sum += count * math.log(uni + bi + tri)
+            log_prob_sum += count * math.log(uni + tri + five)
 
-        n = sum(self.trigrams_train.values())
+        n = sum(self.fivegrams_train.values())
 
         return math.exp(-log_prob_sum / n)
 
     def calculate_dev_perplexity(self, lambda1, lambda2, lambda3):
         log_prob_sum = 0.0
 
-        for trigram, count in self.trigrams_dev.items():
-            unigram, c2, c3 = trigram
-            bigram = unigram+c2
+        for fivegram, count in self.fivegrams_dev.items():
+            c1, c2, c3, c4, unigram = fivegram
+            trigram = c3 + c4 + unigram
+            unigram = unigram if unigram in self.vocab else UNK_CHAR
 
             uni = lambda1 * self.unigram_prob_dev[unigram]
-            bi = lambda2 * self.bigram_prob_dev[bigram]
-            tri = lambda3 * self.trigram_prob_dev[trigram]
+            tri = lambda2 * self.trigram_prob_dev[trigram]
+            five = lambda3 * self.fivegram_prob_dev[fivegram]
 
-            log_prob_sum += count * math.log((uni + bi + tri))
+            log_prob_sum += count * math.log(uni + tri + five)
 
-        n = sum(self.trigrams_dev.values())
+        n = sum(self.fivegrams_dev.values())
 
         return math.exp(-log_prob_sum / n)
 
 
 if __name__ == '__main__':
     ngram = NgramConstructor()
-    print(ngram.get_unigram_train())
-    print(ngram.get_vocab())
-    print(ngram.get_unigram_train_prob())
+    print(ngram.fivegrams_train)
